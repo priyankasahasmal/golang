ee511003bff10fbd81ef46bfd8109a5b581d53ec
priyankasahasmal <sahasmalpriyanka003@gmail.com>
2025-07-04 00:05:41 +0530

    Update Jet config and Go modules

---
+++ jet.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jet -source=postgres -host="dpg-d1ibu73ipnbc73bfqdrg-a.oregon-postgres.render.com" -port="5432" -user="bootcamp_db_qc7b_user" -password="25Zq6NeZxYyLS855V7c9dqbZjKNWU2ZH" -dbname="bootcamp_db_qc7b" -schema="public" -path=./.gen
